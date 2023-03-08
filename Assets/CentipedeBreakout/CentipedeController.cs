686f715b897167e4639683406474218d94f54456
Max <1030163@student.sae.edu.au>
2023-03-08 14:59:20 +0800

    Centipede head movement

--- Assets/CentipedeBreakout/CentipedeController.cs
+++ Assets/CentipedeBreakout/CentipedeController.cs
@@ -1,23 +1,111 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class CentipedeController : MonoBehaviour
+namespace CentipedeBreakout
 {
-    private Collider2D collider;
-    private Rigidbody2D rb;
-
-    // Start is called before the first frame update
-    void Start()
+    public class CentipedeController : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(1, 1);
-    }
+        private Collider2D collider;
+        private Rigidbody2D rb;
 
-    // Update is called once per frame
-    void Update()
-    {
-       
+        private bool fall;
+        private Vector3 horizontal = Vector2.right;
+        private float centipedeHorizontalAngle = 0.1f;
+        private Vector2[] previousPosition;
+        private float distanceMoved;
+        private Vector2 previousFramePosition;
+        
+        public float centipedeSpeedFall;
+        public float centipedeSpeedRise;
+        public float centipedeSpeedHorizontal;
+        
+
+        public GameObject leftWall;
+        public GameObject roof;
+        public GameObject rightWall;
+        public GameObject floor;
+        public GameObject player;
+        public GameObject hit;
+        public GameObject[] centipedeSegments;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                previousPosition[i] = new Vector2(0,2);
+            }
+
+
+            rb = GetComponent<Rigidbody2D>();
+            //Debug.Log("Active");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Fall();
+            HorizontalMove();
+
+            distanceMoved += Mathf.Sqrt(Mathf.Pow(previousFramePosition.x - transform.position.x, 2) - Mathf.Pow(previousFramePosition.y - transform.position.x, 2));
+            previousFramePosition = transform.position;
+            //Debug.Log("Active");
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject == leftWall)
+            {
+                //Should use magnitude but angry ):<
+                centipedeHorizontalAngle *= -1;
+            }
+            if (collision.gameObject == rightWall)
+            {
+                centipedeHorizontalAngle *= -1;
+            }
+            if (collision.gameObject == roof)
+            {
+                Debug.Log("ROOF");
+                fall = true;
+            }
+            if (collision.gameObject == floor)
+            {
+                fall = false;
+            }
+        }
+
+        private float UpdatePositions(float distanceMoved)
+        {
+            
+            while (distanceMoved > 0.1)
+            {
+                
+                distanceMoved -= 0.1f;
+            }
+            return 1f;
+        }
+
+        private void Fall()
+        {
+            if (fall)
+            {
+                Debug.Log("FALL");
+                transform.position += Vector3.down * centipedeSpeedFall + new Vector3(centipedeSegments.Length, 0, 0) * centipedeSpeedFall;
+            }
+            else
+            {
+                transform.position += Vector3.up * centipedeSpeedRise + new Vector3(centipedeSegments.Length, 0, 0) * centipedeSpeedRise;
+            }
+        }
+
+        private void HorizontalMove()
+        {
+            transform.position += Vector3.right * centipedeSpeedHorizontal * centipedeHorizontalAngle;
+        }
+
     }
 }
