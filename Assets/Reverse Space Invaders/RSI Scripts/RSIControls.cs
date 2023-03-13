8e69ceebbee75209a3eb70fe102fc15aab9bf84d
Bookmarkz <pablo.schreyvogel1@gmail.com>
2023-03-13 20:05:14 +0800

    Added some scripts that should but don't work. Also made sprites prefabs

--- Assets/Reverse Space Invaders/RSI Scripts/RSIControls.cs
+++ Assets/Reverse Space Invaders/RSI Scripts/RSIControls.cs
@@ -7,27 +7,31 @@ namespace ArcadeAnarchy
 {
     public class RSIControls : MonoBehaviour
     {
-        public float moveSpeed = 5f;
-        public Rigidbody2D rb;
-        public Animator animator;
+        public float moveSpeed = 5f; // Publically float to determine speed.
+        public Rigidbody2D rb; // Calls the rigid body so that the sprite can be effected physically.
+        public Animator animator; // Calls Unity's in-built animator to animate the sprite this code is attached to.
+        Vector2 movement; // Uses a Vector based movement system. Placeholder, change to rigid body based later
 
-        Vector2 movement;
-
-        // Update is called once per frame
         void Update()
         {
             // Input
             movement.x = Input.GetAxisRaw("Horizontal");
-            //movement.y = Input.GetAxisRaw("Vertical");
-
             animator.SetFloat("Horizontal", movement.x);
-            //animator.SetFloat("Vertical", movement.y);
+
+            /* Find a way to make it so that once hits boundary area, pushes alien swarm down a row.
+            movement.y = Input.GetAxisRaw("Vertical");
+            animator.SetFloat("Vertical", movement.y); */
             animator.SetFloat("Speed", movement.sqrMagnitude);
+
+            // if(rb.position ==  )
+            // {
+
+            // }
         }
 
         void FixedUpdate()
         {
-            // Movement
+            // Executes movement code: checks where sprite is then uses vector movement based on Deltatime.
             rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
         }
     }
