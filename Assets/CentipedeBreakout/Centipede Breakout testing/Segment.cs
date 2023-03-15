d5c86a420546545393bc0f1bc789c9ed3eea13ac
Max <1030163@student.sae.edu.au>
2023-03-15 10:21:00 +0800

    Bounces and moves

--- Assets/CentipedeBreakout/Centipede Breakout testing/Segment.cs
+++ Assets/CentipedeBreakout/Centipede Breakout testing/Segment.cs
@@ -1,140 +1,34 @@
-<<<<<<< HEAD:Assets/CentipedeBreakout/CentipedeController.cs
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
+using CentipedeBreakout;
 
 namespace CentipedeBreakout
 {
-    public class CentipedeController : MonoBehaviour
+
+    public class Segment : MonoBehaviour
     {
-        private Collider2D collider;
-        private Rigidbody2D rb;
-
-        private bool fall;
-        private Vector3 horizontal = Vector2.right;
-        private float centipedeHorizontalAngle = 0.1f;
-        private Vector2[] previousPosition;
-        private float distanceMoved;
-        private Vector2 previousFramePosition;
-        
-        public float centipedeSpeedFall;
-        public float centipedeSpeedRise;
-        public float centipedeSpeedHorizontal;
-        
-
-        public GameObject leftWall;
-        public GameObject roof;
-        public GameObject rightWall;
-        public GameObject floor;
-        public GameObject player;
-        public GameObject hit;
-        public GameObject[] centipedeSegments;
+        public GameObject attachedHead;
 
         // Start is called before the first frame update
         void Start()
         {
-            for (int i = 1; i < 100; i++)
-            {
-                previousPosition[i] = new Vector2(0,2);
-            }
 
+        }
 
-            rb = GetComponent<Rigidbody2D>();
-            //Debug.Log("Active");
+        void OnMouseDown()
+        {
+            Debug.Log(gameObject + "Object clicked");
+            Destroy(gameObject);
         }
 
         // Update is called once per frame
         void Update()
         {
-            Fall();
-            HorizontalMove();
 
-            distanceMoved += Mathf.Sqrt(Mathf.Pow(previousFramePosition.x - transform.position.x, 2) - Mathf.Pow(previousFramePosition.y - transform.position.x, 2));
-            previousFramePosition = transform.position;
-            //Debug.Log("Active");
         }
-
-        private void OnCollisionEnter2D(Collision2D collision)
-        {
-            if (collision.gameObject == leftWall)
-            {
-                //Should use magnitude but angry ):<
-                centipedeHorizontalAngle *= -1;
-            }
-            if (collision.gameObject == rightWall)
-            {
-                centipedeHorizontalAngle *= -1;
-            }
-            if (collision.gameObject == roof)
-            {
-                Debug.Log("ROOF");
-                fall = true;
-            }
-            if (collision.gameObject == floor)
-            {
-                fall = false;
-            }
-        }
-
-        private float UpdatePositions(float distanceMoved)
-        {
-            
-            while (distanceMoved > 0.1)
-            {
-                
-                distanceMoved -= 0.1f;
-            }
-            return 1f;
-        }
-
-        private void Fall()
-        {
-            if (fall)
-            {
-                Debug.Log("FALL");
-                transform.position += Vector3.down * centipedeSpeedFall + new Vector3(centipedeSegments.Length, 0, 0) * centipedeSpeedFall;
-            }
-            else
-            {
-                transform.position += Vector3.up * centipedeSpeedRise + new Vector3(centipedeSegments.Length, 0, 0) * centipedeSpeedRise;
-            }
-        }
-
-        private void HorizontalMove()
-        {
-            transform.position += Vector3.right * centipedeSpeedHorizontal * centipedeHorizontalAngle;
-        }
-
     }
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Segment : MonoBehaviour
-{
-    public GameObject attachedHead;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    void OnMouseDown()
-    {
-        Debug.Log(gameObject + "Object clicked");
-        Destroy(gameObject);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
->>>>>>> MaxsBranch:Assets/CentipedeBreakout/Centipede Breakout testing/Segment.cs
--- Assets/CentipedeBreakout/Centipede Breakout testing/SegmentBody.cs
+++ Assets/CentipedeBreakout/Centipede Breakout testing/SegmentBody.cs
@@ -1,154 +1,225 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+using CentipedeBreakout;
 
-public class SegmentBody : Segment
+namespace CentipedeBreakout
 {
-    //tracks how far apart each body segment is and the values where it can be.
-    private float distanceMoved;
-    public Vector2[] headPositionPrevious;
-    private Vector3 previousFramePosition;
-
-
-    //allows you to edit how long a centipede is, on start.
-    public int numberOfbodySegments;
-    public GameObject bodyPrefab;
-    public float distanceBetweenSegments;
-
-    private GameObject[] bodySegments;
-
-
-    //A counter for where the most recently updated value is, ++ is oldest, -- is newest
-    private int startOfArray = 1;
-    private int beforeStartOfArray;
-    
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class SegmentBody : Segment
     {
-        if (distanceBetweenSegments == 0)
-        {
-            distanceBetweenSegments = 0.1f;
-        }
+        //tracks how far apart each body segment is and the values where it can be.
+        private float distanceMoved;
+        public Vector2[] headPositionPrevious;
+        private Vector3 previousFramePosition;
 
-        
 
-        bodySegments = new GameObject[numberOfbodySegments];
-        //Creates body parts and tells them what head they are attached to
-        for (int i = 0; i < numberOfbodySegments; i++)
+        //allows you to edit how long a centipede is, on start.
+        public int numberOfbodySegments;
+        public GameObject bodyPrefab;
+        public float distanceBetweenSegments;
+
+        private GameObject[] bodySegments;
+
+
+        //A counter for where the most recently updated value is, ++ is oldest, -- is newest
+        private int startOfArray = 1;
+        private int beforeStartOfArray;
+
+        // the code for collision and movemovent
+        public GameObject leftWall;
+        public GameObject roof;
+        public GameObject rightWall;
+        public GameObject floor;
+        public GameObject hit;
+
+        private Collider2D collider;
+        private Rigidbody2D rb;
+
+        private bool fall;
+        private float centipedeHorizontalAngle = 0.1f;
+        public float centipedeSpeedFall;
+        public float centipedeSpeedRise;
+        public float centipedeSpeedHorizontal;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            if (bodySegments[i] == null)
+            //rb = gameObject.AddComponent<Rigidbody2D>();
+
+            if (distanceBetweenSegments == 0)
             {
-                bodySegments[i] = Instantiate(bodyPrefab, new Vector3(-3000, 3000, 0), Quaternion.identity);
+                distanceBetweenSegments = 0.1f;
             }
-            //attached head is called to tell the head if the body part dies
-            //Without it the body part does not know which head it belongs to.
-            bodySegments[i].GetComponent<Segment>().attachedHead = gameObject;
-        }
-        attachedHead = gameObject;
-
-        for (int i = 0; i == bodySegments.Length * 10 - 1; i++)
-        {
-            headPositionPrevious[i] = new Vector2(1000,0);
-        }
-
-        headPositionPrevious = new Vector2[bodySegments.Length * 10];
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        WASDTest();
-
-        
-        UpdatePreviousPositions();
-        DebugVisual();
 
 
-        int i = 1;
-        foreach (var item in bodySegments)
-        {
-            int x = 0;
-            if (startOfArray + i*10 > bodySegments.Length * 10 - 1)
+
+            bodySegments = new GameObject[numberOfbodySegments];
+            //Creates body parts and tells them what head they are attached to
+            for (int i = 0; i < numberOfbodySegments; i++)
             {
-                x = startOfArray + i * 10 - bodySegments.Length * 10;
+                if (bodySegments[i] == null)
+                {
+                    bodySegments[i] = Instantiate(bodyPrefab, new Vector3(-3000, 3000, 0), Quaternion.identity);
+                }
+                //attached head is called to tell the head if the body part dies
+                //Without it the body part does not know which head it belongs to.
+                bodySegments[i].GetComponent<Segment>().attachedHead = gameObject;
+            }
+            attachedHead = gameObject;
+
+            for (int i = 0; i == bodySegments.Length * 10 - 1; i++)
+            {
+                headPositionPrevious[i] = new Vector2(1000, 0);
+            }
+
+            headPositionPrevious = new Vector2[bodySegments.Length * 10];
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            WASDTest();
+
+            Fall();
+            HorizontalMove();
+            UpdatePreviousPositions();
+            DebugVisual();
+
+
+            int i = 1;
+            foreach (var item in bodySegments)
+            {
+                int x = 0;
+                if (startOfArray + i * 10 > bodySegments.Length * 10 - 1)
+                {
+                    x = startOfArray + i * 10 - bodySegments.Length * 10;
+                }
+                else
+                {
+                    x = startOfArray + i * 10;
+                }
+                item.transform.position = headPositionPrevious[x];
+                i++;
+            }
+
+            previousFramePosition = transform.position;
+        }
+
+        private void DebugVisual()
+        {
+
+            for (int i = 0; i < bodySegments.Length * 10 - 1; i++)
+            {
+                Debug.DrawLine(headPositionPrevious[i], headPositionPrevious[i] + new Vector2(0, 0.1f), Color.yellow);
+            }
+
+        }
+
+
+
+        private void UpdatePreviousPositions()
+        {
+            beforeStartOfArray = startOfArray - 1;
+            if (beforeStartOfArray == -1)
+            {
+                beforeStartOfArray = bodySegments.Length * 10 - 1;
+            }
+
+
+            //Remove this code post testing
+            if (distanceBetweenSegments <= 0)
+            {
+                distanceBetweenSegments = 0.01f;
+                Debug.Log("don't");
+            }
+
+
+
+            distanceMoved += Vector3.Distance(transform.position, previousFramePosition);
+            while (distanceMoved >= distanceBetweenSegments)
+            {
+                distanceMoved -= distanceBetweenSegments;
+
+                headPositionPrevious[startOfArray] = transform.position;
+                startOfArray += 1;
+
+                if (startOfArray == bodySegments.Length * 10)
+                {
+                    startOfArray = 0;
+                }
+            }
+
+
+
+
+        }
+
+        //not working???
+        private void WASDTest()
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb.velocity = new Vector3(1,0,0);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb.velocity = new Vector3(-1,0,0);
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb.velocity = new Vector3(0, 1, 0);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                rb.velocity = new Vector3(0, -1, 0);
+            }
+        }
+
+        public void OnCollisionEnter2D(Collision2D collision)
+        {
+            Debug.Log("COLLIDING");
+            if (collision.gameObject == leftWall)
+            {
+                //Should use magnitude but angry ):<
+                centipedeHorizontalAngle *= -1;
+            }
+            if (collision.gameObject == rightWall)
+            {
+                centipedeHorizontalAngle *= -1;
+            }
+            if (collision.gameObject == roof)
+            {
+                Debug.Log("ROOF");
+                fall = true;
+            }
+            if (collision.gameObject == floor)
+            {
+                fall = false;
+            }
+        }
+
+        private void Fall()
+        {
+            if (fall)
+            {
+                //Debug.Log("FALL");
+                //transform.position += Vector3.down * centipedeSpeedFall + new Vector3(0, bodySegments.Length, 0) * centipedeSpeedFall;
+                transform.position += Vector3.down * centipedeSpeedFall;
             }
             else
             {
-                x = startOfArray + i * 10;
-            }
-            item.transform.position = headPositionPrevious[x];
-            i++;
-        }
-
-        previousFramePosition = transform.position;
-
-    }
-
-    private void DebugVisual()
-    {
-        
-        for (int i = 0; i < bodySegments.Length * 10 - 1; i++)
-        {
-            Debug.DrawLine(headPositionPrevious[i], headPositionPrevious[i] + new Vector2(0, 0.1f), Color.yellow);
-        }
-        
-    }
-
-
-    //Known issue, distance updates constantly meaning any amount of movement creates new co-ordinates given enough time
-    //e.g if the centipede is slow enough it will congeal together.
-    private void UpdatePreviousPositions()
-    {
-        beforeStartOfArray = startOfArray -1;
-        if (beforeStartOfArray == -1)
-        {
-            beforeStartOfArray = bodySegments.Length * 10 - 1; 
-        }
-
-     
-        //Remove this code post testing
-        if (distanceBetweenSegments <= 0)
-        {
-            distanceBetweenSegments = 0.01f;
-            Debug.Log("don't");
-        }
-
-        
-
-        distanceMoved += Vector3.Distance(transform.position, previousFramePosition);
-        while (distanceMoved >= distanceBetweenSegments)
-        {
-            distanceMoved -= distanceBetweenSegments;
-            
-            headPositionPrevious[startOfArray] = transform.position; 
-            startOfArray += 1;
-
-            if (startOfArray == bodySegments.Length * 10)
-            {
-                startOfArray = 0;
+                //transform.position += Vector3.up * centipedeSpeedRise + new Vector3(0, bodySegments.Length, 0) * centipedeSpeedRise;
+                transform.position += Vector3.up * centipedeSpeedRise;
             }
         }
 
+        private void HorizontalMove()
+        {
+            transform.position += Vector3.right * centipedeSpeedHorizontal * centipedeHorizontalAngle;
+        }
 
-        
 
     }
-
-    //not working???
-    private void WASDTest()
-    {
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position += new Vector3(0.1f,0,0);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position -= new Vector3(0.1f, 0, 0);
-        }
-    }
-
-    
 }
