8d56bac7a945e4bf78f27373bd17729c5fc72517
George <george.irlgames@gmail.com>
2023-03-19 20:44:41 +0800

    Added some UI Button Functionality

--- Assets/Universal Scripts/UIManager.cs
+++ Assets/Universal Scripts/UIManager.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class UIManager : MonoBehaviour
 {
-
+    public GameObject pauseMenu;
+    public GameObject optionsMenu;
+    public bool isPaused;
     public TextMeshProUGUI uiText;
     public void Awake()
     {
-        ArcadeAnarchy.EventManager.instance.OnUpdateUI += UpdateUI;
+       // ArcadeAnarchy.EventManager.instance.OnUpdateUI += UpdateUI;
     }
 
     public void UpdateUI(string text)
@@ -18,10 +22,25 @@ public class UIManager : MonoBehaviour
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && !isPaused)
         {
-
+            isPaused = true;
+            Time.timeScale = 0f;
+            pauseMenu.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && isPaused)
+        {
+            Resume();
+            optionsMenu.SetActive(false);
         }
     }
+
+    public void Resume()
+    {
+        isPaused = false;
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+    }
+
 }
 
