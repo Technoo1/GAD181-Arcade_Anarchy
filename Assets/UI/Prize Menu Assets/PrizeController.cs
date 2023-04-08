1dfc3f9f80830cbf9251f866dcb95d11ca26b65f
George <george.irlgames@gmail.com>
2023-04-08 14:52:07 +0800

    Working on Save system

--- Assets/UI/Prize Menu Assets/PrizeController.cs
+++ Assets/UI/Prize Menu Assets/PrizeController.cs
@@ -23,7 +23,10 @@ public class PrizeController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (GetComponent<Image>().color == Color.white)
+        {
+            isPurchased = true;
+        }
     }
     public void OnItemClicked()
     {
