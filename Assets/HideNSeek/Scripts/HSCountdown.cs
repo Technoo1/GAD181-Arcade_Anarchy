8701599abfbda039d689e35a3f41f345a2c34e0f
Keegan <1024868@student.sae.edu.au>
2023-04-18 15:15:16 +0800

    Added UI when time runs out, intel adds time, timer gives 5s warning
    A "mission complete" UI screen now appears when time runs out, showing the total intel acquired. Game over scene still triggers after x seconds

--- Assets/HideNSeek/Scripts/HSCountdown.cs
+++ Assets/HideNSeek/Scripts/HSCountdown.cs
@@ -14,6 +14,9 @@ public class HSCountdown : MonoBehaviour
     public AudioSource fiveSecsRemain;
     public bool alreadyPlayed = false;
 
+    public GameObject ingameScr;
+    public GameObject missionCompleteScr;
+
     void Start()
     {
         timerIsRunning = true; //run countdown once active
@@ -35,6 +38,11 @@ public class HSCountdown : MonoBehaviour
             {
                 timerCount = 0; //once the timer reaches zero...
                 timerIsRunning = false; //... stop the timer
+                //ingameScr.SetActive(false); //disable the ingame UI (this breaks the coroutine in here obviously, find a solution?)
+                missionCompleteScr.SetActive(true); //show the "mission complete" UI
+                //ingameScr.GetComponent<AudioSource>().volume = 0.1f; //drop the ingame music to 50% volume
+                ingameScr.GetComponent<AudioSource>().pitch = 1.0f;
+                Time.timeScale = 0; //effectively pauses the game, minus audio
                 StartCoroutine(GameOverScreen());
             }
         }
@@ -67,8 +75,11 @@ public class HSCountdown : MonoBehaviour
     IEnumerator GameOverScreen()
     {
         Debug.Log("time's up! great living color song btw ;)");
-        yield return new WaitForSeconds(3f); //delay before displaying the game over scene
+        yield return new WaitForSecondsRealtime(5f); //delay before displaying the game over scene
         EventManager.instance.TriggerGameOver(); //game over screen
-    }
 
+        //either delete timer and assign a keypress to if statement, to return to start game UI menu...
+        //or return to start menu INSTEAD of the main menu scene
+        //this should mean time out accrues score whereas a game over resets intel score to 0 and player gets nothing?
+    }
 }
--- Assets/HideNSeek/Scripts/HSIntelCollectable.cs
+++ Assets/HideNSeek/Scripts/HSIntelCollectable.cs
@@ -7,11 +7,19 @@ public class HSIntelCollectable : MonoBehaviour
 {
     float intelValue = 1;
     public GameObject audioPrefab;
+    private HSCountdown Countdown; //reference to the timer
+    public int timeAdded;
 
- private void OnDestroy()
+    void Start()
     {
-        HSIntelScore.playerIntel += intelValue;
-        Instantiate(audioPrefab);
+        Countdown = GameObject.FindGameObjectWithTag("HSTimer").GetComponent<HSCountdown>(); //finding the timer to be referenced
+    }
+
+    void OnDestroy()
+    {
+        HSIntelScore.playerIntel += intelValue; //adds to my Intel score
+        Instantiate(audioPrefab); //plays IntelGet audio
+        Countdown.timerCount += timeAdded; //adds x time to the timer
         //Debug.Log("collected " + intelValue + " Intel");
     }
 }
