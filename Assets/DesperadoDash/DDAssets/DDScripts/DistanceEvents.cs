6efa74afa8e641a30b9ed55c554b3293c4b32648
George <george.irlgames@gmail.com>
2023-04-26 14:20:37 +0800

    changed spawn rates

--- Assets/DesperadoDash/DDAssets/DDScripts/DistanceEvents.cs
+++ Assets/DesperadoDash/DDAssets/DDScripts/DistanceEvents.cs
@@ -19,6 +19,7 @@ public class DistanceEvents : MonoBehaviour
 
     public event Action OnThousandMeters; 
     public event Action OnFiveHundredMeters;
+    public event Action OnTwoThousandMeters;
     // Update is called once per frame
     private void Update()
     {
@@ -29,6 +30,10 @@ public class DistanceEvents : MonoBehaviour
             if (scoreManager.count >= 1000f) //if the score/distance is more than or equal to 1000 meters
         {
             OnThousandMeters?.Invoke(); //invoke the event and convey this to all of the listeners to the event
+        }
+            if (scoreManager.count >= 2000f)
+        {
+            OnTwoThousandMeters?.Invoke();
         }
 
     }
